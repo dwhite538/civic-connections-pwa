d24e747fe5324229ab68b28f7829d4177075b443
dwhite538 <dwhitesvp@gmail.com>
2025-08-02 23:49:19 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,10 +8,13 @@ const ASSETS = [
   OFFLINE_URL
 ];
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => 
+      Promise.all(keys.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
   );
 });
 
