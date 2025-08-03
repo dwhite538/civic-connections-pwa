d606615db4f9f58399e5471b63978b69e66101f7
dwhite538 <dwhitesvp@gmail.com>
2025-08-02 23:19:35 -0500

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/civil-connections-pwa/sw.js', { 
+    navigator.serviceWorker.register('/civic-connections-pwa/sw.js', { 
       scope: '/civil-connections-pwa/' 
     })
     .then(reg => console.log('SW registered for scope:', reg.scope))
